225e62b31b7b5604ab3bcbb7f6c1d0b06322a9c8
School of IT <mail2schoolofit@gmail.com>
2024-12-22 14:32:07 +0530

    Day-2-SQL

---
+++ Day_2/SQL_Workout_2025_Day_2_DDL.sql
@@ -0,0 +1,15 @@
+create TABLE school_of_it_2025.gifts (
+    gift_name VARCHAR(100),
+    recipient VARCHAR(100),
+    weight_kg NUMERIC(5, 2)
+);
+
+
+insert into school_of_it_2025.gifts (gift_name, recipient, weight_kg) values
+('Toy Train', 'John', 2.5),
+('Chocolate Box', 'Alice', 0.8),
+('Teddy Bear', 'Sophia', 1.2),
+('Board Game', 'Liam', 0.9);
+
+
+
