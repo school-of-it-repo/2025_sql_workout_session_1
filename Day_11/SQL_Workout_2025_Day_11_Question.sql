f71a658d55da6d76773b8bb7f2b07b1c3d982c0a
School of IT <mail2schoolofit@gmail.com>
2025-01-04 09:40:03 +0530

    Day-11-SQL-Question

---
+++ Day_11/SQL_Workout_2025_Day_11_Question.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA IF NOT EXISTS school_of_it_2025;
+
+CREATE TABLE school_of_it_2025.family_members (
+    member_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    relationship VARCHAR(50) NOT NULL,
+    birthday DATE NOT NULL
+);
+
+-- Insert sample data
+INSERT INTO school_of_it_2025.family_members (member_id, name, relationship, birthday)
+VALUES
+    (1, 'Dawn', 'Sister', '2024-12-24'),
+    (2, 'Bob', 'Father', '2024-05-20'),
+    (3, 'Charlie', 'Brother', '2024-12-25'),
+    (4, 'Diana', 'Mother', '2024-03-15');
+   
+
+	
