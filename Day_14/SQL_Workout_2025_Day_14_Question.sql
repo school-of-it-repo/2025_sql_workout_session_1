9d200f3745a0e9d7a48b2a41a19c73cc24c19ce2
School of IT <mail2schoolofit@gmail.com>
2025-01-07 08:18:29 +0530

    Day-14-SQL-Question

---
+++ Day_14/SQL_Workout_2025_Day_14_Question.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA IF NOT EXISTS school_of_it_2025;
+
+-- Create snowfall table
+CREATE TABLE school_of_it_2025.snowfall_q_14 (
+    resort_name VARCHAR(100) PRIMARY KEY,
+    location VARCHAR(100) NOT NULL,
+    snowfall_inches NUMERIC(5, 2) NOT NULL
+);
+
+-- Insert sample data into snowfall
+INSERT INTO school_of_it_2025.snowfall_q_14 (resort_name, location, snowfall_inches)
+VALUES
+    ('Snowy Peaks', 'Colorado', 60),
+    ('Winter Wonderland', 'Utah', 45),
+    ('Frozen Slopes', 'Alaska', 75);
+
+select * from school_of_it_2025.snowfall_q_14;
+
+
