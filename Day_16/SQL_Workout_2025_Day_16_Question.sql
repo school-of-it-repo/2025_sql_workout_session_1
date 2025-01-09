7243d3ffce6db9c8d0db2b9886197bdea4f2f48f
School of IT <mail2schoolofit@gmail.com>
2025-01-09 07:21:38 +0530

    Day-16-SQL-Question

---
+++ Day_16/SQL_Workout_2025_Day_16_Question.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA IF NOT EXISTS school_of_it_2025;
+
+-- Create candy_sales table
+CREATE TABLE school_of_it_2025.candy_sales (
+    sale_id SERIAL PRIMARY KEY,
+    candy_name VARCHAR(100) NOT NULL,
+    quantity_sold INTEGER NOT NULL CHECK (quantity_sold >= 0),
+    price_per_unit NUMERIC(5, 2) NOT NULL CHECK (price_per_unit >= 0),
+    category VARCHAR(50) NOT NULL
+);
+
+-- Insert sample data into candy_sales
+INSERT INTO school_of_it_2025.candy_sales (sale_id, candy_name, quantity_sold, price_per_unit, category)
+VALUES
+    (1, 'Candy Cane', 20, 1.5, 'Sweets'),
+    (2, 'Chocolate Bar', 10, 2, 'Chocolate'),
+    (3, 'Lollipop', 5, 0.75, 'Sweets'),
+    (4, 'Dark Chocolate Truffle', 8, 2.5, 'Chocolate'),
+    (5, 'Gummy Bears', 15, 1.2, 'Sweets'),
+    (6, 'Chocolate Fudge', 12, 3, 'Chocolate');
+   
+ select * from school_of_it_2025.candy_sales;
+
+
+
+
