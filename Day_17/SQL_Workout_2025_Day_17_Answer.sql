9c095803971e5681f8340675f6582d8e05729d88
School of IT <mail2schoolofit@gmail.com>
2025-01-10 21:15:22 +0530

    Day-17-SQL-Answer

---
+++ Day_17/SQL_Workout_2025_Day_17_Answer.sql
@@ -0,0 +1,30 @@
+CREATE SCHEMA IF NOT EXISTS school_of_it_2025;
+
+
+CREATE TABLE school_of_it_2025.grinch_pranks (
+    prank_id SERIAL PRIMARY KEY,
+    prank_name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    difficulty VARCHAR(50) CHECK (difficulty IN ('Beginner', 'Advanced', 'Expert'))
+);
+
+-- Insert sample data
+INSERT INTO school_of_it_2025.grinch_pranks (prank_name, location, difficulty) VALUES
+('Stealing Stockings', 'Whoville', 'Beginner'),
+('Christmas Tree Topple', 'Whoville Town Square', 'Advanced'),
+('Present Swap', 'Cindy Lous House', 'Beginner'),
+('Sleigh Sabotage', 'Mount Crumpit', 'Expert'),
+('Chimney Block', 'Mayors Mansion', 'Expert');
+
+
+select * from school_of_it_2025.grinch_pranks;
+
+
+SELECT prank_name,
+       location
+FROM   school_of_it_2025.grinch_pranks
+WHERE  difficulty IN( 'Advanced', 'Expert' )
+ORDER  BY 1 DESC,
+          2 DESC ;
+
+
