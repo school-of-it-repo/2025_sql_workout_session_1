9cdfed98248fe62a87c50dbc36d36af6c41670d8
School of IT <mail2schoolofit@gmail.com>
2025-01-14 21:39:27 +0530

    Day-21-SQL-Question-Answer

---
+++ Day_21/SQL_Workout_2025_Day_21_Answer.sql
@@ -0,0 +1,30 @@
+CREATE SCHEMA IF NOT EXISTS school_of_it_2025;
+
+
+CREATE TABLE school_of_it_2025.gifts_q_21 (
+    gift_id SERIAL PRIMARY KEY,
+    gift_name VARCHAR(255) NOT NULL,
+    recipient_type VARCHAR(50) NOT NULL CHECK (recipient_type IN ('good', 'naughty')),
+    weight_kg NUMERIC(5, 2) NOT NULL CHECK (weight_kg > 0)
+);
+
+
+INSERT INTO school_of_it_2025.gifts_q_21 (gift_name, recipient_type, weight_kg) VALUES
+('Toy Train', 'good', 2.5),
+('Lumps of Coal', 'naughty', 1.5),
+('Teddy Bear', 'good', 1.2),
+('Chocolate Bar', 'good', 0.3),
+('Board Game', 'naughty', 1.8);
+
+
+select * from school_of_it_2025.gifts_q_21;
+
+
+select 
+	recipient_type
+	,sum(weight_kg) as total_weight	
+	,round((sum(weight_kg)/ sum(sum(weight_kg)) over()),2) * 100 weight_percentage
+
+from 
+	school_of_it_2025.gifts_q_21
+group by recipient_type
