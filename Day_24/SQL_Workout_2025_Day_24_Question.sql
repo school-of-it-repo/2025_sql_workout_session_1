5da7bc916acfc1400f59e218411f39d172e9eec2
School of IT <mail2schoolofit@gmail.com>
2025-01-17 07:49:56 +0530

    Day-24-SQL-Question

---
+++ Day_24/SQL_Workout_2025_Day_24_Question.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS school_of_it_2025;
+
+CREATE TABLE school_of_it_2025.deliveries (
+    delivery_date DATE NOT NULL,
+    gifts_delivered INT NOT NULL
+);
+
+
+INSERT INTO school_of_it_2025.deliveries (delivery_date, gifts_delivered) VALUES
+('2024-12-20', 120),
+('2024-12-21', 150),
+('2024-12-22', 200),
+('2024-12-23', 300),
+('2024-12-24', 500);
+
+
+select * from school_of_it_2025.deliveries ;
+
+
+select 
+    delivery_date
+    ,gifts_delivered
+    ,sum(gifts_delivered) over(order by delivery_date ) running_total
+from 
+school_of_it_2025.deliveries;
